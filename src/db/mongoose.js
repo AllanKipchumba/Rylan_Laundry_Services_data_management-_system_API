6a3bfd94f7dca480a89db2f1f6a5d3d73f7976c5
allan <allankipchumba76@gmail.com>
2023-03-28 18:39:58 +0300

    switched to production db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,13 @@
 const config = require("../config/env/config");
-// const { mongoDB_url_local, mongoDB_url_prod } = config.mongoDB_url_local;
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-mongoose.connect(`mongodb://127.0.0.1/Rylan_laundry_services`, {
+const mongoDB_url_prod = config.mongoDB_url_prod;
+
+// mongoose.connect(`mongodb://127.0.0.1/Rylan_laundry_services`, {
+//   useNewUrlParser: true,
+// });
+mongoose.connect(`${mongoDB_url_prod}`, {
   useNewUrlParser: true,
 });
