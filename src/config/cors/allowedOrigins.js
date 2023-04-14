06bcd9672c91ed774e1c63fea847ec16b933547c
allan <allankipchumba76@gmail.com>
2023-04-14 21:21:26 +0300

    whitelisted static site on render to avoid requests being blocked by CORS

--- src/config/cors/allowedOrigins.js
+++ src/config/cors/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://dashboard-rylan-laundry-dms.vercel.app",
   "http://localhost:5173",
+  "https://dashboard-rylan-laundry-dms.onrender.com",
 ];
 
 module.exports = allowedOrigins;
